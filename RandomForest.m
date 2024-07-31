f7454f190cc34dcf5fc5cb769a96c3f4e38a4c3d
HadasGanan <hadass314@gmail.com>
2024-07-31 21:47:22 +0300

    /

--- RandomForest.m
+++ RandomForest.m
@@ -1,49 +1,39 @@
 clc
 close all force
+trained = 0;
+%uncomment to reset training data
 trainingData = load("trainingData.mat").trainingData;
 trainingResult = load("trainingResult.mat").trainingResult;
-pca_len = 3;
-dataFile = "tree(4-17).mat";
-imageFile = "tree(4-17).png";
-[MM, normalized, ~] = loadAndNormalize(dataFile); 
-firstBand = 1;
-lastBand = 100;
-MM = MM(:, :, firstBand:lastBand);
-workingMatrix = MM;
-% pca = hyperpca(normalized,pca_len);
-
-%uncomment to reset training data
+pca = load("pca.mat").pca;
 % trainingData = [];
 % trainingResult = [];
 while questdlg("collect more data?") == "Yes"
-    [trainingDataTemp, trainingResultTemp] = createTrainingData(imageFile, workingMatrix); %and here
-    if questdlg("add the data to the training set?") == "Yes"
-        trainingData = cat(1, trainingData, trainingDataTemp);
-        trainingResult = cat(1, trainingResult, trainingResultTemp);
-    end
+    [trainingDataTemp, trainingResultTemp, pca] = createTrainingData("tree(4-11).mat", "tree(4-11).png");
+    trainingData = cat(1, trainingData, trainingDataTemp);
+    trainingResult = cat(1, trainingResult, trainingResultTemp);
 end
 %uncomment to save training data
-save('trainingData.mat', 'trainingData')
-save('trainingResult.mat', 'trainingResult')
-sizepca = size(workingMatrix);
+% save('trainingData.mat', 'trainingData')
+% save('trainingResult.mat', 'trainingResult')
+sizepca = size(pca);
 Mdl = TreeBagger(100, trainingData, trainingResult, Method="classification", OOBPrediction="on");
-disp(Mdl);
-% view(Mdl.Trees{1},Mode="graph")
-% plot(oobError(Mdl))
-% xlabel("Number of Grown Trees")
-% ylabel("Out-of-Bag Classification Error")
-p = reshape(workingMatrix, [sizepca(1) * sizepca(2), sizepca(3)]);
+view(Mdl.Trees{1},Mode="graph")
+plot(oobError(Mdl))
+xlabel("Number of Grown Trees")
+ylabel("Out-of-Bag Classification Error")
+p = reshape(pca, [sizepca(1) * sizepca(2), sizepca(3)]);
 y = predict(Mdl, p);
 y_square = str2double(reshape(y, sizepca(1), sizepca(2)));
 
-I = imread(imageFile);
-I = imresize(I, [512 512]);
-
 figure
-subplot(1,2,1), image(I),title('RGB image')
+subplot(1,2,1),image(imread("tree(4-11).png")),title('RGB image')
 colormap("sky")
 subplot(1,2,2),imagesc(y_square),title('predicted')
 datacursormode on
+
+% figure
+% rescalePC = rescale(reducedDataCube,0,1);
+% montage(rescalePC,'BorderSize',[10 10],'Size',[1 sizepca(3)]);
 
 % Count the number of zeros in the binary matrix y_square
 numZeros = sum(y_square(:) == 1);
@@ -62,15 +52,13 @@ disp(['Number of zeros in y_square: ', num2str(numZeros)]);
 disp(['Percentage of zeros in y_square: ', num2str(percentageZeros), '%']);
 
 
-% figure
-% rescalePC = rescale(pca,0,1);
-% montage(rescalePC,'BorderSize',[10 10],'Size',[1 sizepca(3)]);
 
-
-function [trainingData, trainingResult] = createTrainingData(imgFile, pca)
+function [trainingData, trainingResult, pca] = createTrainingData(dataMatrix, imgFile)
+    pca_len = 5;
+    [~, normalized, ~] = loadAndNormalize(dataMatrix);
+    reducedDataCube = hyperpca(normalized,pca_len);
+  
     I = imread(imgFile);
-    I = imresize(I, [512 512]);
-     s = size(pca);
     title("select region and double click inside");
     [J,rect] = imcrop(I);
     imshow(J)
@@ -84,17 +72,23 @@ function [trainingData, trainingResult] = createTrainingData(imgFile, pca)
     for i = 1:length(x_flower)
         m(y_flower(i), x_flower(i)) = 1;
     end
-    data = zeros(1, width * height, s(3));
+    data = zeros(1, width * height, pca_len);
     result = zeros(width * height, 1);
     counter = 0;
     for y = yoffset:yoffset + height - 1
         for x = xoffset:xoffset + width - 1
             counter = counter + 1;
-            data(1, counter, :) = pca(y, x, :);
+            data(1, counter, :) = reducedDataCube(y, x, :);
             result(counter) = m(y + 1 - yoffset, x + 1 - xoffset) == 1;
         end
     end
-   
-    trainingData = reshape(data, [width * height, s(3)]);
+    
+    trainingData = reshape(data, [width * height, pca_len]);
     trainingResult = result;
+    pca = reducedDataCube;
 end
+
+
+
+
+
